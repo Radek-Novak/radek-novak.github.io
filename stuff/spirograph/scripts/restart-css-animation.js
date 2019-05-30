b528a8aa66f7a80acef7442392b9c015911af5cd
Radek Novak <radek.novak.cze@gmail.com>
2019-05-30 18:36:19 +0200

    autocommit 2019-05-30 18:36:03

--- stuff/spirograph/scripts/restart-css-animation.js
+++
@@ -1,8 +0,0 @@
-// http://css-tricks.com/restart-css-animation/
-
-function resetElem(sel) {
- var el = $(sel),  
- newone = el.clone(true);
- el.before(newone);
- $("." + el.attr("class") + ":last").remove();
-}
