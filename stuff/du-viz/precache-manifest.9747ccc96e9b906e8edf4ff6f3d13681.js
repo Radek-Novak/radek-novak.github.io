759f0148c6d85ef5989c3a02962af99963aded76
Radek Novak <radek.novak.cze@gmail.com>
2019-07-10 23:24:41 +0200

    autocommit 2019-07-10 23:24:40

---
+++ stuff/du-viz/precache-manifest.9747ccc96e9b906e8edf4ff6f3d13681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996ab815a29ac006d90dba1da17a0ac4",
+    "url": "/stuff/du-viz/index.html"
+  },
+  {
+    "revision": "57218492127c73d988a7",
+    "url": "/stuff/du-viz/static/css/main.9169e522.chunk.css"
+  },
+  {
+    "revision": "c39b4516284374972143",
+    "url": "/stuff/du-viz/static/js/2.a015b095.chunk.js"
+  },
+  {
+    "revision": "57218492127c73d988a7",
+    "url": "/stuff/du-viz/static/js/main.4eba487f.chunk.js"
+  },
+  {
+    "revision": "cf62c24ffb9663ddf824",
+    "url": "/stuff/du-viz/static/js/runtime~main.16b95462.js"
+  }
+]);
